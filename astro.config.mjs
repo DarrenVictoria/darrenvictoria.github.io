bffe6520ce199117ad71bc4ad93761d4f60dce91
DarrenVictoria <darrenvictoria2@gmail.com>
2024-05-03 23:59:00 +0530

    Remove tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://darrenvictoria.github.io',
-  integrations: [tailwind()]
+  
   
 
 });
